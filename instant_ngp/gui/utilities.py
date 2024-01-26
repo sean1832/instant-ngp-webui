217f53e9a5614a1556c03089318f847b5488d0b5
Zeke Zhang <sean1832725142@gmail.com>
2024-01-27 00:37:28 +1100

    fix: cannot find `data` directory when it doesn't exists

--- instant_ngp/gui/utilities.py
+++ instant_ngp/gui/utilities.py
@@ -1,7 +1,10 @@
+import os
+
+
 def write_video_to_disk(video, path):
     with open(path, "wb") as f:
         f.write(video.getbuffer())
 
 
 def is_dir_empty(path):
-    return not any(path.iterdir())
+    return not os.path.exists(path) or not any(path.iterdir())
